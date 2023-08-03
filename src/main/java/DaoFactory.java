0964597b9274283b47f174a5afb25a539c296a94
devinsi <leclay.yo@gmail.com>
2023-08-03 11:09:27 +0200

    release1.2 create DaoFactory

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -5,27 +5,35 @@ import java.sql.SQLException;
 public class DaoFactory {
 	
 	//URL de connexion
-	public static String url = "jdbc:postgresql://localhost:5434/GeometricForm";
-	
+	private String url;  //= "jdbc:postgresql://localhost:5434/GeometricForm";
 	//nom du user
-	private static String user = "admin";
-	
+	private String user; //= "postgres";
 	//mot de passe du user
-	private static String mdp = "admin";
+	private String mdp; //= "admin";
 	
-	//Objet connexion
-	private static Connection connection;
+	DaoFactory(String url, String user, String mdp){
+		this.url = url;
+		this.user = user; 
+		this.mdp = mdp; 
 	
-	//Méthode qui va nous retourner notre instance de connxion
-	//et la c réer si elle n'existe pas 
-	public static Connection getInstance() {
-		if(connection == null) {
-			try{
-                connection = DriverManager.getConnection(url, user, mdp);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }        
-        return connection;  		
-	}
+	//Méthode qui va nous retourner notre instance de connexion
+	//et la créer si elle n'existe pas 
+	public static DaoFactory getInstance() {
+		try {
+			Class.forName("org.postgresql.Driver");
+		}catch(ClassNotFoundException e) {
+			
+		}
+		
+		DaoFactory instance = new DaoFactory("jdbc:postgresql://localhost:5434/GeometricForm", "postgres", "admin");
+		
+	public Connection getConnection() throws SQLException {
+	        return DriverManager.getConnection(url, user, mdp);
+	    }
+
+    // Récupération du Dao
+    public CubeDao getUtilisateurDao() {
+        return new CubeDaoImpl(this);
+    }		
+    
 }
